ddc60a71a287cb07709364776587f81fac9fa788
Mirco Heck <m.heck@gebhardt-group.com>
2022-06-13 17:36:16 +0200

    Ah darn.. there was a staged commit...

--- php/templates/site.php
+++ php/templates/site.php
@@ -8,7 +8,8 @@
    <meta name="keywords" content="Smirnoff Kids, Band, Haßmersheim, Mosbach, Metal, Rock, Musik">
    <meta name="author" content="Mirco Heck, Levin Beier">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" type="text/css" href="/css/main.css" media="(min-width: 1280px)" />
+   <link rel="stylesheet" type="text/css" href="/css/common.css" />
+   <link rel="stylesheet" type="text/css" href="/css/desktop.css" media="(min-width: 1280px)" />
    <link rel="stylesheet" type="text/css" href="/css/mobile.css" media="(max-width: 1280px)" />
    <link rel="icon" type="image/x-icon" href="favicon.ico">
    <title>Smirnoff Kids - Home</title>
@@ -16,33 +17,37 @@
 
 <body>
 
-   <div id="main_container">
+   <main>
       <header>
-         <a href="index.php">
+         <a href="index.html">
             <img src="/assets/images/sm_logo.png" id="logo">
          </a>
       </header>
       <nav>
-         <a href="index.php" class="MenuLink" style="text-decoration: underline;">Home</a>-
-         <a href="konzerte.php" class="MenuLink">Konzerte</a>-
-         <a href="bilder.php" class="MenuLink">Bilder</a>-
-         <a href="kontakt.php" class="MenuLink">Kontakt</a>-
-         <a href="impressum.php" class="MenuLink">Impressum</a>
+         <a href="index.html" class="MenuLink" id="ActiveLink">Home</a><span class="seperator">-</span>
+         <a href="konzerte.html" class="MenuLink">Konzerte</a><span class="seperator">-</span>
+         <a href="bilder.html" class="MenuLink">Bilder</a><span class="seperator">-</span>
+         <a href="kontakt.html" class="MenuLink">Kontakt</a><span class="seperator">-</span>
+         <a href="impressum.html" class="MenuLink">Impressum</a>
       </nav>
 
-      <div id="seperator"></div>
+      <hr>
 
-      <main>
+      <section>
+         
+      </section>
 
-      </main>
-      <footer>
-         Instagram: <a href="https://www.instagram.com/smirnoffkids_official/">www.instagram.com/smirnoffkids_official</a><br>
-         Facebook: <a href="https://de-de.facebook.com/pages/category/Musician-Band/Smirnoff-Kids-211068725738369/">www.facebook.de/pages/category/Musician-Band/Smirnoff-Kids-211068725738369/</a><br>
-         Email: <a href="mailto:kontakt@smirnoff-kids.de">kontakt@smirnoff-kids.de</a>
-         <br><br>
-         Website made by Mirco & Levin
-      </footer>
-   </div>
+      <pre>
+         <footer>
+Instagram: <a href="https://www.instagram.com/smirnoffkids_official/">www.instagram.com/smirnoffkids_official</a>
+Facebook: <a href="https://de-de.facebook.com/pages/category/Musician-Band/Smirnoff-Kids-211068725738369/">www.facebook.de/pages/category/Musician-Band/Smirnoff-Kids-211068725738369/</a>
+Email: <a href="mailto:kontakt@smirnoff-kids.de">kontakt@smirnoff-kids.de</a>
+
+
+<b>Website made by Mirco & Levin</b>
+         </footer>
+      </pre>
+   </main>
 
 </body>
 
